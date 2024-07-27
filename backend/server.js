a4aa7fbd3ef3adcf53dbfe93158ac9ee177d2eb7
Yashas Singh <ysingh2026@gmail.com>
2024-07-27 12:59:50 -0400

    removed mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 // backend/server.js
 
 const express = require('express');
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -10,11 +9,6 @@ const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
-
-// Database connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error(err));
 
 app.get('/', (req, res) => {
     res.send('API is running...');
